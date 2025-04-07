cf7c7d62eb3a4603488aa49920705f3b277d8e9b
NamanNekiye321 <namannekiye321@gmail.com>
2025-04-07 18:22:28 +0530

    commit

---
+++ Chapter-3/if_basic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a;
+
+    printf("Enter a number\n");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+        printf("Even\n");
+    }
+    else
+    {
+        printf("Odd\n");
+    }
+
+    return 0;
+}
