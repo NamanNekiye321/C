9ff10a1e18933b9c6d4e24e142f1e57ef91d0611
NamanNekiye321 <namannekiye321@gmail.com>
2025-04-08 20:49:44 +0530

    commit

--- Chapter-3/if_else_if.c
+++ Chapter-3/if_else_if.c
@@ -2,7 +2,30 @@
 
 int main() {
     
-    
+    int num;
+
+    printf("Enter your number:\n");
+    scanf("%d",&num);
+
+    if(num == 1){
+        printf("Your number is 1\n");
+    }
+    else if(num == 2){ 
+        printf("Your number is 2\n");
+    }
+    else if(num == 3){
+        printf("Your number is 3\n");
+    }
+    else if(num == 4){
+        printf("Your number is 4\n");
+    }
+    else{
+        printf("No idea\n");
+    }
+
+
+
+
 
 
 
